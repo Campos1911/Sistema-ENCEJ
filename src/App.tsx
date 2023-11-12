dea45b6aa8c9cb9882432dbb96527cb6c22af2f8
Campos1911 <anthonithomes@gmail.com>
2023-11-12 09:59:30 -0300

    Tela de selecionar quartos iniciada

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Home from './pages/Inicio/inicio';
+import SelecQuarto from './pages/SelecionarQuarto/selecQuarto';
 
 /**O ARQUIVO DECLARATION.D.TS FOI CRIADO PARA CORRIGIR UM BUG NAS IMAGENS**/
 
 function App() {
   return (
     <>
-    <Home />
+    <SelecQuarto />
     </>
   );
 }
---
+++ src/pages/SelecionarQuarto/selecQuarto.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import './style.css'
+
+import Encej from '../../images/Logo/Logo.jpg';
+import Legenda from '../../images/Legenda/Legenda.png'
+import Button from "../../components/Button/button";
+
+export default function SelecQuarto() {
+    return(
+        <>
+        <header className="header1">
+
+            <div className="boxUser">
+                <img className="fotoUser" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Foto de Perfil" />
+                <div className="columInfos">
+                    <h1>Carlos R. Guimarães</h1>
+                    <p>CT Junior</p>
+                </div>
+            </div>
+
+            <div className="endLogo">
+                <img className="logoEncej" src={Encej} alt="Logo Encej" />
+            </div>
+
+        </header>
+        
+        <section className="boxLegenda">
+            <img className="legenda" src={Legenda} alt="legendaQuartos" />
+        </section>
+
+        <div className="selecCapacidade">
+            <h2>Selecione a capacidade do quarto:</h2>
+            <div className="centralizarButtons">
+                <div className="botaoQuartos">
+                    <Button classname="4Lugares" insideText="4 Lugares" />
+                    <Button classname="6Lugares" insideText="6 Lugares" />
+                    <Button classname="10Lugares" insideText="10 Lugares" />
+                </div>
+            </div>
+        </div>
+
+        <section className="quartos">
+
+        </section>
+        
+        <footer className="organizadores">
+
+        </footer>
+
+        </>
+    );
+}
