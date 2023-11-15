d4ae1a9373430b5b14c12b47b82aab96efafc08b
rafaxxix <149822230+rafaxxix@users.noreply.github.com>
2023-11-15 03:27:44 -0300

    adição básica dos componentes

--- src/pages/BoasVindas/App.tsx
+++ src/pages/BoasVindas/App.tsx
@@ -8,11 +8,25 @@ export default function boasVindas () {
   return (
     <>
 
-<Header username="Carlos Teste" empresaJ="CT Junior"/>
+      <Header username="Carlos Teste" empresaJ="CT Junior"/>
 
-<Footer />
+        <section classname="centralizarTitulo">
+          <h1><strong>Boas vindas,</strong></h1>
+        </section>
+
+        <p classname="centralizarParagrafo">
+          Você deverá escolher um quarto dentre os disponibilizados. Caso <br/>
+          queira trocar de quarto, basta escolher outro quarto que têm vagas <br/>
+          disponíveis até a data limite. Para ver detalhadamente as <br/>
+          informações de cada quarto, escolha a opção “ver quartos”.
+        </p>
         
-
+        <div classname="centralizarButtons">
+          <Button classname='bttWelcome' insideText='Ver quartos'/>
+        </div>
+      
+      <Footer />
+        
     </>
   );
 }
