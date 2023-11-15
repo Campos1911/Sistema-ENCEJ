68f96a1f1d011c2b571b28a173368ec9587330a5
rafaxxix <149822230+rafaxxix@users.noreply.github.com>
2023-11-15 01:34:01 -0300

    iniciada pagina boas vindas

---
+++ src/pages/BoasVindas/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+import Header from "../../components/Header/headerUser";
+import Footer from "../../components/Footer/footer";
+
+export default function boasVindas () {
+  return (
+    <>
+
+<Header username="Carlos Teste" empresaJ="CT Junior"/>
+
+<Footer />
+        
+
+    </>
+  );
+}
