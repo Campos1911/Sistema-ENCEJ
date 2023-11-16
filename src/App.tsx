6a6a4a5ac5df9cd18d4b96def99cfced9351092e
Campos1911 <anthonithomes@gmail.com>
2023-11-15 21:50:40 -0300

    correção de bugs na pasta de boas vindas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Home from './pages/Inicio/inicio';
 import SelecQuarto from './pages/SelecionarQuarto/selecQuarto';
+import BoasVindas from './pages/BoasVindas/boasVindas';
 
 /**O ARQUIVO DECLARATION.D.TS FOI CRIADO PARA CORRIGIR UM BUG NAS IMAGENS**/
 
 function App() {
   return (
     <>
-    <SelecQuarto />
+    <BoasVindas />
     </>
   );
 }
--- src/pages/BoasVindas/boasVindas.tsx
+++ src/pages/BoasVindas/boasVindas.tsx
@@ -3,25 +3,26 @@ import './style.css';
 
 import Header from "../../components/Header/headerUser";
 import Footer from "../../components/Footer/footer";
+import Button from '../../components/Button/button';
 
-export default function boasVindas () {
+export default function BoasVindas () {
   return (
     <>
 
       <Header username="Carlos Teste" empresaJ="CT Junior"/>
 
-        <section classname="centralizarTitulo">
+        <section className="centralizarTitulo">
           <h1><strong>Boas vindas,</strong></h1>
         </section>
 
-        <p classname="centralizarParagrafo">
+        <p className="centralizarParagrafo">
           Você deverá escolher um quarto dentre os disponibilizados. Caso <br/>
           queira trocar de quarto, basta escolher outro quarto que têm vagas <br/>
           disponíveis até a data limite. Para ver detalhadamente as <br/>
           informações de cada quarto, escolha a opção “ver quartos”.
         </p>
         
-        <div classname="centralizarButtons">
+        <div className="centralizarButtons">
           <Button classname='bttWelcome' insideText='Ver quartos'/>
         </div>
       
