51db10eefd876c78e406ab0dec1e822adaf53df6
rafaxxix <149822230+rafaxxix@users.noreply.github.com>
2023-11-16 17:56:30 -0300

    andamento componente de pop-up

--- src/components/Container Sucesso/containerSucesso.tsx
+++ src/components/Container Sucesso/containerSucesso.tsx
@@ -2,9 +2,26 @@ import React from "react";
 import "./style.css";
 
 
-export default function ContainerQuarto({quarto}:{quarto: string}) {
+export default function ContainerSucesso({username, empresaJ}:{username:string, empresaJ:string}) {
     return (
         <>
+        /**EH PRECISO RECEBER INFO DE QUANTIDADE DE PESSOAS QUE CABEM NO QUARTO**/
+        <div className="whiteBox">
+
+            /**RECEBER NUMERO DO QUARTO**/
+
+            <h1>Você está no Quarto 5!</h1>
+
+            /**ICONE STATUS DO QUARTO **/
+            
+            <div className="boxUser">
+                <img className="fotoUser" src={Avatar} alt="Foto de Perfil" />
+                <div className="columInfos">
+                    <h2>{username}</h2>
+                    <p>{empresaJ}</p>
+                </div>
+            </div>
+        </div>
         
         </>
     );
