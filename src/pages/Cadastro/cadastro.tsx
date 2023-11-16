20511c190c8bdad0d38d1e87192f7d844f0e0cdc
Campos1911 <anthonithomes@gmail.com>
2023-11-15 21:57:25 -0300

    edição na opção de selecionar a EJ

--- src/pages/Cadastro/cadastro.tsx
+++ src/pages/Cadastro/cadastro.tsx
@@ -14,11 +14,11 @@ export default function Cadastro() {
                     <h1>Registro de Participante</h1>
                     <p>Insira seus dados para prosseguir</p>
 
-                    <div className="boxInputs">
+                    <form className="boxInputs">
                         <Input type="text" placeh="Nome do participante" classname="username"/>
                         <Input type="password" placeh="Insira sua senha" classname="password"/>
                         <Input type="password" placeh="Confirme sua senha" classname="password"/>
-                    </div>
+                    </form>
                     
                     <select className="selecEJ">
                         <option value="ej1" selected>CT Junior</option>
