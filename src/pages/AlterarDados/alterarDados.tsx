14681854b466c2686a8a9f2f5cf57b38d0d4dcff
Anthoni Campos <anthonithomes@gmail.com>
2023-11-15 23:00:23 -0300

    página pré-pronta

--- src/pages/AlterarDados/alterarDados.tsx
+++ src/pages/AlterarDados/alterarDados.tsx
@@ -2,16 +2,29 @@ import React from "react";
 import "./style.css";
 import Header from "../../components/Header/headerUser";
 import Input from "../../components/Input/input";
+import Button from "../../components/Button/button";
+import Footer from "../../components/Footer/footer";
 
 export default function AlterarDados() {
     return(
         <>
             <Header username="Carlos Teste" empresaJ="CT Junior"/>
 
-            <Input type="text" placeh="Insira seu nome" classname="username" />
-            <Input type="password" placeh="Insira sua senha" classname="password" />
-            <Input type="text" placeh="Insira seu nome" classname="username" />
+            <section className="text">
+                <h1>Alteração de dados do Usuário</h1>
+                <p>Insira seus novos dados abaixo</p>
+            </section>
 
+            <section className="centralizar">
+                <Input type="text" placeh="Insira seu nome" classname="username" />
+                <Input type="password" placeh="Insira sua senha" classname="password" />
+                <Input type="text" placeh="Insira seu nome atualizado" classname="newUsername" />
+                <Input type="text" placeh="Insira seu novo meio de Contato" classname="newContact" />
+                <Button insideText="Salvar" classname="bttn1" />
+            </section>
+
+            <Footer />
+            
         </>
     );
 }
