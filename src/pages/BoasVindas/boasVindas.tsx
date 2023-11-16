886a6e344daf7fa98c0f3f18b77aa53ac3edf05e
rafaxxix <149822230+rafaxxix@users.noreply.github.com>
2023-11-15 22:42:05 -0300

    correçao de bugs

--- src/pages/BoasVindas/boasVindas.tsx
+++ src/pages/BoasVindas/boasVindas.tsx
@@ -3,26 +3,21 @@ import './style.css';
 
 import Header from "../../components/Header/headerUser";
 import Footer from "../../components/Footer/footer";
-import Button from '../../components/Button/button';
 
-export default function BoasVindas () {
+export default function boasVindas () {
   return (
     <>
 
       <Header username="Carlos Teste" empresaJ="CT Junior"/>
 
-        <section className="centralizarTitulo">
+        <section classname="centralizarTitulo">
           <h1><strong>Boas vindas,</strong></h1>
         </section>
 
-        <p className="centralizarParagrafo">
+        <p classname="centralizarParagrafo">
           Você deverá escolher um quarto dentre os disponibilizados. Caso <br/>
           queira trocar de quarto, basta escolher outro quarto que têm vagas <br/>
           disponíveis até a data limite. Para ver detalhadamente as <br/>
-          informações de cada quarto, escolha a opção “ver quartos”.
-        </p>
-        
-        <div className="centralizarButtons">
           <Button classname='bttWelcome' insideText='Ver quartos'/>
         </div>
       
