6a491201444b00a774cbd5072d6f04098dbea081
rafaxxix <149822230+rafaxxix@users.noreply.github.com>
2023-11-17 01:32:18 -0300

    alterada família de fonte e adicionados componentes de 4, 6 e 10 quartos para containers de popup

---
+++ src/components/ContainerQuarto10P/containerQuarto10P.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import "./style.css";
+
+import Avatar from "../../images/Organizadores/Avatar.jpg" 
+
+
+export default function ContainerSucesso({username, empresaJ}:{username:string, empresaJ:string}) {
+    return (
+        <>
+        {/**EH PRECISO RECEBER INFO DE QUANTIDADE DE PESSOAS QUE CABEM NO QUARTO**/}
+
+            {/**RECEBER NUMERO DO QUARTO**/}
+        <div className="boxRoom">
+            <div className="title">
+                <h1>Você está no Quarto 5!</h1>
+            </div>
+
+            {/**ICONE STATUS DO QUARTO **/}
+            {/**QUANDO CHAMAR ESSA FUNCAO PASSAR USERNAME E EMPRESAJ */}
+        <div className="column">
+            <div className="item">
+                <img className="fotoUsuario" src={Avatar} alt="Foto de Perfil" />
+                <div className="infos">
+                    <h2>{username}Carlos Teste</h2>
+                    <p>{empresaJ}CT Junior</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img className="fotoUsuario" src={Avatar} alt="Foto de Perfil" />
+                <div className="infos">
+                    <h2>{username}Carlos Teste</h2>
+                    <p>{empresaJ}CT Junior</p>
+                </div>
+            </div>
+        </div>
+               
+        <div className="column">
+            <div className="item">
+                <img className="fotoUsuario" src={Avatar} alt="Foto de Perfil" />
+                <div className="infos">
+                    <h2>{username}Carlos Teste</h2>
+                    <p>{empresaJ}CT Junior</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img className="fotoUsuario" src={Avatar} alt="Foto de Perfil" />
+                <div className="infos">
+                    <h2>{username}Carlos Teste</h2>
+                    <p>{empresaJ}CT Junior</p>
+                </div>
+            </div>
+        </div>
+
+        <div className="column">
+            <div className="item">
+                <img className="fotoUsuario" src={Avatar} alt="Foto de Perfil" />
+                <div className="infos">
+                    <h2>{username}Carlos Teste</h2>
+                    <p>{empresaJ}CT Junior</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img className="fotoUsuario" src={Avatar} alt="Foto de Perfil" />
+                <div className="infos">
+                    <h2>{username}Carlos Teste</h2>
+                    <p>{empresaJ}CT Junior</p>
+                </div>
+            </div>
+        </div>
+
+        <div className="column">
+            <div className="item">
+                <img className="fotoUsuario" src={Avatar} alt="Foto de Perfil" />
+                <div className="infos">
+                    <h2>{username}Carlos Teste</h2>
+                    <p>{empresaJ}CT Junior</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img className="fotoUsuario" src={Avatar} alt="Foto de Perfil" />
+                <div className="infos">
+                    <h2>{username}Carlos Teste</h2>
+                    <p>{empresaJ}CT Junior</p>
+                </div>
+            </div>
+        </div>
+
+        <div className="column">
+            <div className="item">
+                <img className="fotoUsuario" src={Avatar} alt="Foto de Perfil" />
+                <div className="infos">
+                    <h2>{username}Carlos Teste</h2>
+                    <p>{empresaJ}CT Junior</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img className="fotoUsuario" src={Avatar} alt="Foto de Perfil" />
+                <div className="infos">
+                    <h2>{username}Carlos Teste</h2>
+                    <p>{empresaJ}CT Junior</p>
+                </div>
+            </div>
+        </div>
+
+        </div>
+        </>
+    );
+    }
