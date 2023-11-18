fb87b0e101432e1eb3121f3403cf228f57377a15
rafaxxix <149822230+rafaxxix@users.noreply.github.com>
2023-11-18 00:42:42 -0300

    criadas paginas de sucesso, correção de bugs nos componentes desse memso pop up, tudo nas 3 versões de display, adição de itens na pasta images

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Home from './pages/Inicio/inicio';
 import SelecQuarto from './pages/SelecionarQuarto/selecQuarto';
-import ContainerSucesso from './components/ContainerQuarto10P/containerQuarto10P';
+import Sucesso6P from './pages/Sucesso6P/successSix'
 
 /**O ARQUIVO DECLARATION.D.TS FOI CRIADO PARA CORRIGIR UM BUG NAS IMAGENS**/
 
 function App() {
   return (
     <>
-    <ContainerSucesso username='' empresaJ='' />
+    <Sucesso6P/>
     </>
   );
 }
