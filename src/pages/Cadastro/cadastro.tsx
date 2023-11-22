260b03f7c4c6f99157755393a3a276715eaca9f3
rafaxxix <149822230+rafaxxix@users.noreply.github.com>
2023-11-21 23:48:51 -0300

    tentativa de integração

--- src/pages/Cadastro/cadastro.tsx
+++ src/pages/Cadastro/cadastro.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Logo from "../../images/Logo/Logo.jpg";
 import "./cadastro.css"
 import Input from "../../components/Input/input";
@@ -6,6 +8,35 @@ import Button from "../../components/Button/button";
 import Footer from "../../components/Footer/footer";
 
 export default function Cadastro() {
+
+    const [teste, setTeste] = useState ([])
+    const navigate = useNavigate ()
+    const URL = 'https://wldzajo7ka.execute-api.us-east-1.amazonaws.com/dev/' 
+
+      async function handleLogin () {
+        const response = await fetch (`${URL}person`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify ({
+            "email": {teste},
+            "senha": "1234",
+            "nome": "Alex",
+            "empresa": "CT"
+          
+                    })
+                  })
+            return(navigate("/boas-vindas"))
+          }
+
+          function handleLogin1(event: any) {
+
+            setTeste(event.target.value)
+            console.log(teste)
+          }
+
+
     return (
         <>
             {/**DIV PARA CENTRALIZAR ELEMENTOS NO MEIO DA PÁGINA**/}
@@ -20,12 +51,13 @@ export default function Cadastro() {
                     {/**FORMULÁRIO PARA PEGAR DADOS DO PARTICIPANTE**/}
                     <form className="boxInputs">
                         <Input type="text" placeh="Nome do participante" classname="username"/>
+                        <input value={teste}  onChange={(event) => handleLogin1(event)} type="teste" placeholder="Email do participante" id="username1" />
                         <Input type="password" placeh="Insira sua senha" classname="password"/>
                         <Input type="password" placeh="Confirme sua senha" classname="password"/>
                     </form>
 
                     <div className="ajustaButton">
-                        <Button caminho="boas-vindas" insideText="Registrar" classname="bttnCadastro" />
+                        <button className="bttnCadastro" onClick={handleLogin}>Registrar</button>
                     </div>                    
 
                     <div className="responsividadeFooterCadastro">
--- src/pages/Inicio/inicio.tsx
+++ src/pages/Inicio/inicio.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import './inicio.css'
-import { useNavigate } from 'react-router-dom'
+import './inicio.css';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 /**IMPORT DAS IMAGENS**/
 import Encej from '../../../src/images/Logo/Logo.jpg'
